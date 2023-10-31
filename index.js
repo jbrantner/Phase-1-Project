dd71d1c605fc4a7ebee6f36d5d914f8290783d00
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-31 14:04:08 -0400

    findConsonants function returns the consonants of the word inputted

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ let leftOvers = [].join('')
 
 let test = []
 
-function removeVowels(word){
+function findConsonants(word){
     // split's string into array
     const letters = word.split('')
-    const vowels = [];
-    const consonants = []
+    const newLetters = [...letters]
 
     // goes through each element of the letters array
-    for(let ii = 0; ii < letters.length; ii++){
+    for(let ii = 0; ii < newLetters.length; ii++){
         // goes through vowels array and compares element to each vowel letter
         for(let i = 0; i < vowelsCheck.length; i++){
-            // if element of letters === a vowel letter, then push that letter to the vowel array
-            if(letters[ii] === vowelsCheck[i]){
-                vowels.push(letters[ii])
-                console.log(vowels)
+            // if element of letters === a vowel letter, then replace vowel with ' '
+            if(newLetters[ii] === vowelsCheck[i]){
+                letters.splice(ii, 1, ' ')
+                console.log(letters)
             }
         }
-        // if it doesn't find a vowel, push the letter to the consonants array
-        
     }
+    // removes spaces and turns consonants variable into a string
+    const consonants = letters.filter((letter) => letter !== ' ').join('')
+    return consonants;
+
 }
 
-removeVowels('lets see how this goes')
+findConsonants('lets see how this goes')
 
-console.log(test)
+
 
 
