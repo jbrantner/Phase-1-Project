f5c51ab799f40cb7fb976c4f8ce34b3b96abae03
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-31 14:22:06 -0400

    added countVowels function that countst he number of vowels in a word

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const vowelsCheck = ['a', 'e', 'i', 'o', 'u']
 
 let leftOvers = [].join('')
 
-
-let test = []
-
 function findConsonants(word){
     // split's string into array
     const letters = word.split('')
@@ -19,7 +16,6 @@ function findConsonants(word){
             // if element of letters === a vowel letter, then replace vowel with ' '
             if(newLetters[ii] === vowelsCheck[i]){
                 letters.splice(ii, 1, ' ')
-                console.log(letters)
             }
         }
     }
@@ -29,7 +25,49 @@ function findConsonants(word){
 
 }
 
-findConsonants('lets see how this goes')
+function countVowels(word){
+    // split's string into array
+    const letters = word.split('')
+    const newLetters = [...letters]
+    let vowels = []
+
+    // goes through each element of the letters array
+    for(let ii = 0; ii < newLetters.length; ii++){
+        // goes through vowels array and compares element to each vowel letter
+        for(let i = 0; i < vowelsCheck.length; i++){
+            // if element of letters === a vowel letter, then push vowel to vowels variable
+            if(newLetters[ii] === vowelsCheck[i]){
+                vowels.push(newLetters[ii])
+            }
+        }
+    }
+    return vowels.length;
+}
+
+// function findVowels(word){
+//     // split's string into array
+//     const letters = word.split('')
+//     const newLetters = [...letters]
+//     let vowels = []
+
+//     // goes through each element of the letters array
+//     for(let ii = 0; ii < newLetters.length; ii++){
+//         // goes through vowels array and compares element to each vowel letter
+//         for(let i = 0; i < vowelsCheck.length; i++){
+//             // if element of letters === a vowel letter, then push vowel to vowels variable
+//             if(newLetters[ii] === vowelsCheck[i]){
+//                 vowels.push(newLetters[ii])
+//             }
+//         }
+//     }
+//     return vowels;
+// }
+
+
+console.log('count vowels = ' + countVowels('lets see how this goes'))
+console.log('find con = ' + findConsonants('lets see how this goes'))
+
+
 
 
 
