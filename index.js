f625954fc40a7f8a6ff5f16f31364729de3eb174
Jordan Brantner <thejordanbrantner@gmail.com>
2023-11-02 12:16:21 -0400

    created a container that should be able to be deleted by the dom. Created some functions for finding the vowels in an inputted word.

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 function fetchClient(){
     let number = 0
-
-// Async Code
-
+// Async Code //
 fetch("clients.json")
     // convert to json
     .then(function (response){return response.json()})
@@ -13,29 +11,41 @@ fetch("clients.json")
         number = getRandomClient(data)
         return data
     })
-    // use random number to access client's information
-    .then((data) => {accessClientData(data, number)})
+    // add data and number to various functions
+    .then((data) => {postClientData(data, number)})
+// end Async Code // 
 
-// end Async Code
-
-    
-// generates a random client
 function getRandomClient(data){
     const clientNumber = data.length
     return Math.floor(Math.random() * clientNumber);
 }}
 
-// accesses age and vowels from that random number index
-function accessClientData(data, number) {
-    console.log(data[number]);
+
+
+function postClientData(data, number) {
+    // creates profile container p
+    const profileContainer = document.createElement('p')
+    profileContainer.id = `${number}`
+    document.getElementById('clientProfiles').appendChild(profileContainer)
+
+    // creates profile h1
     const createProfile = document.createElement('h1')
-    document.getElementById('clientProfiles').appendChild(createProfile);
     createProfile.innerHTML = `Hello, my name is ${data[number].name}. I'm ${data[number].age} years old. I would like to order '${data[number].likes[0].toUpperCase()}' and '${data[number].likes[1].toUpperCase()}', please.`
+    createProfile.id = `client${number}`
+    profileContainer.appendChild(createProfile)
+    // creates textbox input
     const orderInput = document.createElement('input')
-    document.getElementById('clientProfiles').appendChild(orderInput);
+    orderInput.id = `client${number}`
+    orderInput.className = 'inputButton'
+    profileContainer.appendChild(orderInput);
+    // creates button to submit text
     const orderInputButton = document.createElement('button')
     orderInputButton.innerHTML = "Submit Order"
-    document.getElementById('clientProfiles').appendChild(orderInputButton)
+    profileContainer.appendChild(orderInputButton)
+
+    orderInputButton.addEventListener('click', () => {
+        findMatchingVowels(data, number, orderInput.value)
+    })
 }
 
 
@@ -46,11 +56,10 @@ const vowelsCheck = ['a', 'e', 'i', 'o', 'u']
 
 let leftOvers = [].join('')
 
-function findConsonants(word){
+function returnConsonants(word){
     // split's string into array
     const letters = word.split('')
     const newLetters = [...letters]
-
     // goes through each element of the letters array
     for(let ii = 0; ii < newLetters.length; ii++){
         // goes through vowels array and compares element to each vowel letter
@@ -58,16 +67,34 @@ function findConsonants(word){
             // if element of letters === a vowel letter, then replace vowel with ' '
             if(newLetters[ii] === vowelsCheck[i]){
                 letters.splice(ii, 1, ' ')
-            }
-        }
-    }
+            }}}
     // removes spaces and turns consonants variable into a string
     const consonants = letters.filter((letter) => letter !== ' ').join('')
     return consonants;
-
 }
 
 function countVowels(word){
+    // split's string into array
+    const letters = word.split('')
+    const newLetters = [...letters]
+    let vowels = []
+    // goes through each element of the letters array
+    for(let ii = 0; ii < newLetters.length; ii++){
+        // goes through vowels array and compares element to each vowel letter
+        for(let i = 0; i < vowelsCheck.length; i++){
+            // if element of letters === a vowel letter, then push vowel to vowels variable
+            if(newLetters[ii] === vowelsCheck[i]){
+                vowels.push(newLetters[ii])
+            }
+        }
+    }
+    return vowels.length;
+}
+
+
+
+
+function returnVowels(word){
     // split's string into array
     const letters = word.split('')
     const newLetters = [...letters]
@@ -83,39 +110,18 @@ function countVowels(word){
             }
         }
     }
-    return vowels.length;
+    return vowels;
 }
-
-// function findVowels(word){
-//     // split's string into array
-//     const letters = word.split('')
-//     const newLetters = [...letters]
-//     let vowels = []
-
-//     // goes through each element of the letters array
-//     for(let ii = 0; ii < newLetters.length; ii++){
-//         // goes through vowels array and compares element to each vowel letter
-//         for(let i = 0; i < vowelsCheck.length; i++){
-//             // if element of letters === a vowel letter, then push vowel to vowels variable
-//             if(newLetters[ii] === vowelsCheck[i]){
-//                 vowels.push(newLetters[ii])
-//             }
-//         }
-//     }
-//     return vowels;
-// }
 
 // consonant button 
 const input = document.getElementById('wordInput');
-
 const button = document.getElementById('countButton')
 button.addEventListener('click', () => {
-    const result = findConsonants(input.value)
+    const result = returnConsonants(input.value)
     const newH1 = document.createElement('h1')
     document.getElementById('result').appendChild(newH1)
     newH1.innerHTML = result
 })
-
 // generate client button
 const generateClientButton = document.getElementById('clientButton')
 // show client profile
@@ -125,5 +131,23 @@ generateClientButton.addEventListener('click', () => {
 
 
 
+// find if vowels match the likes key in the clients object
+function findMatchingVowels(data, number, word){
+    let vowel1 = false;
+    let vowel2 = false;
+    // if user input == value, then return true
+    const vowels = returnVowels(word);
+    vowels.find((element) => {
+        if(element == data[number].likes[0]) {
+            vowel1 = true;
+        }})
+    vowels.find((element) => {
+        if(element == data[number].likes[1]) {
+                vowel2 = true;
+        }})
+        if(vowel1 === true && vowel2 === true){
+            removeClient();
+        }
 
+}
 
